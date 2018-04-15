5efabbc77fd3e3c2fe52a614324f128feeeba8f1
Ramazan <r.ramazanmutlu@gmail.com>
2018-04-15 20:39:06 +0300

    Ödev2 Bitti

--- Ödev2/Ödev1/AnaForm.Designer.cs
+++ Ödev2/Ödev1/AnaForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ödev1
 {
-    partial class Form1
+    partial class AnaForm
     {
         /// <summary>
         ///Gerekli tasarımcı değişkeni.
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.basamakSayisi = new System.Windows.Forms.NumericUpDown();
             this.basamak7 = new System.Windows.Forms.TextBox();
@@ -44,7 +45,23 @@
             this.bLabel2 = new System.Windows.Forms.Label();
             this.basamak1 = new System.Windows.Forms.TextBox();
             this.bLabel1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioTekrarsiz = new System.Windows.Forms.RadioButton();
+            this.radioTekrarli = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.baslaBtn = new System.Windows.Forms.Button();
+            this.hakSayisiUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hakSuresiUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.hakSayisiLbl = new System.Windows.Forms.Label();
+            this.kalanSureLbl = new System.Windows.Forms.Label();
+            this.sureSayac = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.basamakSayisi)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.hakSayisiUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hakSuresiUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -86,6 +103,7 @@
             this.basamak7.Name = "basamak7";
             this.basamak7.Size = new System.Drawing.Size(26, 20);
             this.basamak7.TabIndex = 2;
+            this.basamak7.TextChanged += new System.EventHandler(this.basamak_TextChanged);
             // 
             // bLabel7
             // 
@@ -102,6 +120,7 @@
             this.basamak6.Name = "basamak6";
             this.basamak6.Size = new System.Drawing.Size(26, 20);
             this.basamak6.TabIndex = 2;
+            this.basamak6.TextChanged += new System.EventHandler(this.basamak_TextChanged);
             // 
             // bLabel6
             // 
@@ -118,6 +137,7 @@
             this.basamak5.Name = "basamak5";
             this.basamak5.Size = new System.Drawing.Size(26, 20);
             this.basamak5.TabIndex = 2;
+            this.basamak5.TextChanged += new System.EventHandler(this.basamak_TextChanged);
             // 
             // bLabel5
             // 
@@ -134,6 +154,7 @@
             this.basamak4.Name = "basamak4";
             this.basamak4.Size = new System.Drawing.Size(26, 20);
             this.basamak4.TabIndex = 2;
+            this.basamak4.TextChanged += new System.EventHandler(this.basamak_TextChanged);
             // 
             // bLabel4
             // 
@@ -150,6 +171,7 @@
             this.basamak3.Name = "basamak3";
             this.basamak3.Size = new System.Drawing.Size(26, 20);
             this.basamak3.TabIndex = 2;
+            this.basamak3.TextChanged += new System.EventHandler(this.basamak_TextChanged);
             // 
             // bLabel3
             // 
@@ -166,6 +188,7 @@
             this.basamak2.Name = "basamak2";
             this.basamak2.Size = new System.Drawing.Size(26, 20);
             this.basamak2.TabIndex = 2;
+            this.basamak2.TextChanged += new System.EventHandler(this.basamak_TextChanged);
             // 
             // bLabel2
             // 
@@ -182,6 +205,7 @@
             this.basamak1.Name = "basamak1";
             this.basamak1.Size = new System.Drawing.Size(26, 20);
             this.basamak1.TabIndex = 2;
+            this.basamak1.TextChanged += new System.EventHandler(this.basamak_TextChanged);
             // 
             // bLabel1
             // 
@@ -191,11 +215,158 @@
             this.bLabel1.Size = new System.Drawing.Size(26, 10);
             this.bLabel1.TabIndex = 3;
             // 
-            // Form1
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioTekrarsiz);
+            this.groupBox1.Controls.Add(this.radioTekrarli);
+            this.groupBox1.Location = new System.Drawing.Point(16, 159);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(87, 67);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sayı Türü";
+            // 
+            // radioTekrarsiz
+            // 
+            this.radioTekrarsiz.AutoSize = true;
+            this.radioTekrarsiz.Checked = true;
+            this.radioTekrarsiz.Location = new System.Drawing.Point(7, 43);
+            this.radioTekrarsiz.Name = "radioTekrarsiz";
+            this.radioTekrarsiz.Size = new System.Drawing.Size(68, 17);
+            this.radioTekrarsiz.TabIndex = 0;
+            this.radioTekrarsiz.TabStop = true;
+            this.radioTekrarsiz.Text = "Tekrarsız";
+            this.radioTekrarsiz.UseVisualStyleBackColor = true;
+            // 
+            // radioTekrarli
+            // 
+            this.radioTekrarli.AutoSize = true;
+            this.radioTekrarli.Location = new System.Drawing.Point(7, 20);
+            this.radioTekrarli.Name = "radioTekrarli";
+            this.radioTekrarli.Size = new System.Drawing.Size(60, 17);
+            this.radioTekrarli.TabIndex = 0;
+            this.radioTekrarli.TabStop = true;
+            this.radioTekrarli.Text = "Tekrarlı";
+            this.radioTekrarli.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Hak Sayısı:";
+            // 
+            // baslaBtn
+            // 
+            this.baslaBtn.Location = new System.Drawing.Point(19, 233);
+            this.baslaBtn.Name = "baslaBtn";
+            this.baslaBtn.Size = new System.Drawing.Size(215, 34);
+            this.baslaBtn.TabIndex = 6;
+            this.baslaBtn.Text = "Başla";
+            this.baslaBtn.UseVisualStyleBackColor = true;
+            this.baslaBtn.Click += new System.EventHandler(this.baslaBtn_Click);
+            // 
+            // hakSayisiUpDown
+            // 
+            this.hakSayisiUpDown.Location = new System.Drawing.Point(176, 166);
+            this.hakSayisiUpDown.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.hakSayisiUpDown.Name = "hakSayisiUpDown";
+            this.hakSayisiUpDown.Size = new System.Drawing.Size(58, 20);
+            this.hakSayisiUpDown.TabIndex = 7;
+            this.hakSayisiUpDown.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(112, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hak Süresi:";
+            // 
+            // hakSuresiUpDown
+            // 
+            this.hakSuresiUpDown.Location = new System.Drawing.Point(176, 192);
+            this.hakSuresiUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.hakSuresiUpDown.Name = "hakSuresiUpDown";
+            this.hakSuresiUpDown.Size = new System.Drawing.Size(58, 20);
+            this.hakSuresiUpDown.TabIndex = 7;
+            this.hakSuresiUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Kalan Hakıınız :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Kalan Süreniz :";
+            // 
+            // hakSayisiLbl
+            // 
+            this.hakSayisiLbl.AutoSize = true;
+            this.hakSayisiLbl.Location = new System.Drawing.Point(103, 91);
+            this.hakSayisiLbl.Name = "hakSayisiLbl";
+            this.hakSayisiLbl.Size = new System.Drawing.Size(13, 13);
+            this.hakSayisiLbl.TabIndex = 9;
+            this.hakSayisiLbl.Text = "0";
+            // 
+            // kalanSureLbl
+            // 
+            this.kalanSureLbl.AutoSize = true;
+            this.kalanSureLbl.Location = new System.Drawing.Point(103, 118);
+            this.kalanSureLbl.Name = "kalanSureLbl";
+            this.kalanSureLbl.Size = new System.Drawing.Size(13, 13);
+            this.kalanSureLbl.TabIndex = 9;
+            this.kalanSureLbl.Text = "0";
+            // 
+            // sureSayac
+            // 
+            this.sureSayac.Interval = 1000;
+            this.sureSayac.Tick += new System.EventHandler(this.sureSayac_Tick);
+            // 
+            // AnaForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.kalanSureLbl);
+            this.Controls.Add(this.hakSayisiLbl);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.hakSuresiUpDown);
+            this.Controls.Add(this.hakSayisiUpDown);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.baslaBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.bLabel1);
             this.Controls.Add(this.bLabel2);
             this.Controls.Add(this.bLabel3);
@@ -212,9 +383,13 @@
             this.Controls.Add(this.basamak7);
             this.Controls.Add(this.basamakSayisi);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Name = "AnaForm";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.basamakSayisi)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.hakSayisiUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hakSuresiUpDown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -238,6 +413,19 @@
         private System.Windows.Forms.Label bLabel2;
         private System.Windows.Forms.TextBox basamak1;
         private System.Windows.Forms.Label bLabel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioTekrarsiz;
+        private System.Windows.Forms.RadioButton radioTekrarli;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button baslaBtn;
+        private System.Windows.Forms.NumericUpDown hakSayisiUpDown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown hakSuresiUpDown;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label hakSayisiLbl;
+        private System.Windows.Forms.Label kalanSureLbl;
+        private System.Windows.Forms.Timer sureSayac;
     }
 }
 
---
+++ Ödev2/Ödev1/AnaForm.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ödev1
+{
+    public partial class AnaForm : Form
+    {
+        int MAX_TEXTBOX_COUNT = 7;
+        int[] basamakDizi;
+        bool oyunBasladi = false;
+        int hakSayisi = 0;
+        int kalanSure = 0;
+        int bilinenBasamakSayisi = 0;
+
+        public AnaForm()
+        {
+            InitializeComponent();
+            basamakSayisiKontrol();
+            basamakDizi = rastgeleSayiDizisiUret((int)basamakSayisi.Value, true);
+        }
+
+        private void basamakSayisi_ValueChanged(object sender, EventArgs e)
+        {
+            basamakDizi = rastgeleSayiDizisiUret((int)basamakSayisi.Value, true);
+            basamakSayisiKontrol();
+        }
+        private void basamakSayisiKontrol()
+        {
+            int currentActiveTextbox = (int)basamakSayisi.Value;
+            for (int i = 1; i <= MAX_TEXTBOX_COUNT; i++)
+            {
+                if (i > currentActiveTextbox)
+                {
+                    Controls["basamak" + i].Enabled = false;
+                    Controls["bLabel" + i].Enabled = false;
+                }
+                else
+                {
+                    Controls["basamak" + i].Enabled = true;
+                    Controls["bLabel" + i].Enabled = true;
+                }
+            }
+        }
+        private void basamakDegerKontrol(int basamakId,int[] sayilar)
+        {
+            TextBox basamak = (TextBox)Controls["basamak" + basamakId];
+            bool dizideVar=false;
+            bool dogruYerde = false;
+            if (sayilar[basamakId - 1] == int.Parse(basamak.Text)) dogruYerde = true;
+            else
+            {
+                for (int i = 0; i < sayilar.Length; i++)
+                {
+                    if (sayilar[i] == int.Parse(basamak.Text)) dizideVar = true;
+                }
+            }
+            if (dogruYerde) Controls["bLabel" + basamakId].BackColor = Color.Blue;
+            else if(dizideVar) Controls["bLabel" + basamakId].BackColor = Color.Red;
+
+    
+        }
+        private int[] rastgeleSayiDizisiUret(int basamakSayisi,bool tekrarli)
+        {
+            Random r = new Random();
+            int basamak = 0;
+            int[] donecekDizi=new int[basamakSayisi];
+            for (; basamak < basamakSayisi; basamak++)
+            {
+                if (tekrarli) { donecekDizi[basamak] = (int)Math.Abs(r.NextDouble() * 10); }
+                else
+                {
+                    bool flag = true;
+                    int sayi=-1;
+                    while (flag)
+                    {
+                        flag = false;
+                        sayi= (int)Math.Abs(r.NextDouble() * 10);
+                        for (int i = 0; i < basamak; i++) {
+                            if (sayi == donecekDizi[i]) flag = true;
+                        }
+                    }
+                    donecekDizi[basamak] = sayi;
+                }
+            }
+            return donecekDizi;
+        }
+
+        private void basamak_TextChanged(object sender, EventArgs e)
+        {
+            
+            TextBox tb = (TextBox)sender;
+            if (!oyunBasladi)
+            {
+                tb.Clear();
+                return;
+            }
+            int basamakId = int.Parse(tb.Name.Substring(7));
+            if (tb.Text != "")
+            {
+                hakSayisi--;
+                hakSayisiLbl.Text = hakSayisi.ToString();
+                basamakDegerKontrol(basamakId, basamakDizi);
+                bilinenBasamakSayisi = 0;
+                for (int i = 1; i <= basamakDizi.Length; i++)
+                {
+                    if (Controls["bLabel" + (i)].BackColor == Color.Blue) bilinenBasamakSayisi++;
+                }
+                if (bilinenBasamakSayisi == basamakDizi.Length)
+                {
+                    sureSayac.Stop();
+                    MessageBox.Show("Tebrikler, Bildiniz.");
+                    DialogResult dr = new DialogResult();
+                    dr = MessageBox.Show("Devam etmek istiyor musunuz?", "", MessageBoxButtons.YesNo);
+                    if (dr == DialogResult.No) Application.Exit();
+                    else if (dr == DialogResult.Yes) yeniOyun();
+                }
+            }
+            else Controls["bLabel"+basamakId].BackColor = Color.Yellow;
+        }
+
+        private void baslaBtn_Click(object sender, EventArgs e)
+        {
+            int basamakSayisiInt = (int)basamakSayisi.Value;
+            basamakDizi = rastgeleSayiDizisiUret(basamakSayisiInt, true);
+            Button b = (Button)sender;
+            if (oyunBasladi)
+            {
+                b.Text = "Başla";
+                oyunBasladi = false;
+                sureSayac.Stop();
+            }
+            else
+            {
+                b.Text = "Durdur";
+                sureSayac.Start();
+                oyunBasladi = true;
+                basamakDizi = rastgeleSayiDizisiUret((int)basamakSayisi.Value, true);
+
+                hakSayisi = (int)hakSayisiUpDown.Value;
+                kalanSure = (int)hakSuresiUpDown.Value;
+                hakSayisiLbl.Text = hakSayisi.ToString();
+                kalanSureLbl.Text = kalanSure.ToString();
+            }
+
+        }
+
+        private void sureSayac_Tick(object sender, EventArgs e)
+        {
+            if (kalanSure == 0)
+            {
+                hakSayisi--;
+                hakSayisiLbl.Text = hakSayisi.ToString();
+                kalanSure= (int)hakSuresiUpDown.Value;
+            }
+            if (hakSayisi == 0)
+            {
+                string cevapString = "";
+                foreach (var numara in basamakDizi)
+                    cevapString += numara.ToString();
+                sureSayac.Stop();
+                MessageBox.Show("Hakkınız Dolmuştur.\n Cevap:"+cevapString+" olacaktı.");
+                DialogResult dr = new DialogResult();
+                dr = MessageBox.Show("Devam etmek istiyor musunuz?","",MessageBoxButtons.YesNo);
+                if (dr == DialogResult.No) Application.Exit();
+                else if (dr == DialogResult.Yes) yeniOyun();
+    
+            }
+            kalanSure--;
+            kalanSureLbl.Text = kalanSure.ToString();
+        }
+        private void yeniOyun()
+        {
+            oyunBasladi = false;
+            baslaBtn.Text= "Başla";
+            for (int i = 1; i <= 7; i++)
+            {
+                TextBox tb = (TextBox)Controls["basamak" + i];
+                tb.Clear();
+                Controls["bLabel" + i].BackColor = Color.Yellow;
+                kalanSure = 0;
+                kalanSureLbl.Text = "";
+                hakSayisiLbl.Text = "";
+            }
+        }
+    }
+}
